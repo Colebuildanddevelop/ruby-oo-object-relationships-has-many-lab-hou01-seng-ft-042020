03b12e3eae13c16da24367bdf42da148e2fc3657
colebuildanddevelop <colebuildanddevelop@gmail.com>
2020-04-23 14:33:45 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+class Artist 
+  attr_accessor :name, :songs
+  @@all = []
+  def initialize(name) 
+    @name = name 
+    @songs = []
+  end 
+  def add_song(song) 
+    song.artist = self
+    @songs << song
+  end
+  def add_song_by_name(song_name)
+    song = Song.new(song_name) 
+    song.artist = self
+  end
+  def self.song_count 
+    Song.all.length
+  end
+   
+
+end 
---
+++ lib/post.rb
@@ -0,0 +1,28 @@
+class Post 
+  attr_reader :title, :author
+  @@all = []
+  def initialize(title)
+    @title = title 
+    @@all << self 
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author=(author)
+    author.posts << self
+    @author = author
+  end
+
+  def author_name
+    if @author
+      @author.name
+    else 
+      nil
+    end
+  end
+
+
+
+end
